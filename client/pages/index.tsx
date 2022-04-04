4f17cde2dec9f97f444df672c76b6fc9c75ad71b
eich1cyfaill <hu4lifefllwer@gmail.com>
2022-04-04 21:05:10 +0300

    working on apollo feature

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { client } from './apollo-client'
-import {gql, useQuery} from '@apollo/client'
+// import { client } from './apollo-client'
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client'
 import styles from '../styles/Home.module.css'
 import {Main} from "../layouts/main";
+import {any} from "prop-types";
 
 
 
@@ -30,11 +31,83 @@ const Home: NextPage = ({launches}: any) => {
 
 export async function getStaticProps() {
 
-    const launches = [1, 1, 1]
+    const client = new ApolloClient({
+        uri: 'https://graphql.anilist.co/',
+        cache: new InMemoryCache()
+    })
+
+    /*const { data } = await client.query({
+        query: gql`
+            query {
+                Page(page: 5, perPage: 2){
+                    media {
+                        genres, isAdult, popularity, status
+                    }
+                }
+            }
+        `
+    });*/
+
+    let query = `
+            query{
+                Media (id: 1, type: ANIME) {
+                    id
+                    title {
+                        romaji
+                        english
+                        native
+                    }
+                }
+            }
+    `
+
+    let variables = {
+        id: 15125
+    }
+
+    let url = 'https://graphql.anilist.co',
+        options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query: query,
+                variables: variables
+            })
+        }
+
+    const data = fetch(url, options).then(handleResponse)
+        .then(handleData)
+        .catch(handleError);
+
+    function handleResponse(response: any) {
+        return response.json().then(function (json: any) {
+            return response.ok ? json : Promise.reject(json);
+        });
+    }
+
+    let result
+
+    function handleData(data: any) {
+        result = data
+        console.log(data);
+    }
+
+
+
+
+    function handleError(error: any) {
+        console.error(error);
+    }
+
+
+
 
     return {
         props: {
-            launches
+            result
         }
     }
 }
